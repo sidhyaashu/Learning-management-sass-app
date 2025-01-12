7905aba9f2fcd38a82e01116fca9e05adbd079d6
sidhyaashu <ashutoshsidhya69@gmail.com>
2025-01-12 14:22:15 +0530

    done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,12 +2,10 @@ import { serve } from "inngest/next";
 import { CreateNewUser, GenerateNotes, GenerateStudyTypeContent } from "@/inngest/functions";
 import { inngest } from "@/inngest/client";
 
-export const runtime = 'edge'
 
 // Ensure functions are correctly imported and defined
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    streaming:'allow',
     functions: [
         CreateNewUser,
         GenerateNotes,
